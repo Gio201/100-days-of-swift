bfff40bf7874623358df9a49ea17b87b65753aee
Giovanni Berlijn <>
2024-09-09 14:39:37 +0200

    Day 1 - 100 Days of SwiftUI

---
+++ 100daysofswiftui/Day 1.swift
@@ -0,0 +1,116 @@
+//
+//  Day 1.swift
+//  100daysofswiftui
+//
+//  Created by WeMa Intern on 09/09/2024.
+//
+
+import Foundation
+
+//How to create variables and constants
+
+var greeting "Hello, playground."
+
+var name "Ted"
+name = "Rebecca"
+name = "Kelly"
+
+let character = "Daphne"
+
+let characters = "Daphne"
+character "Eloise"
+character "Frans"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+let managerName = "Frank"
+let dogBread = "Golden retriever"
+let gameName = "Elden Ring"
+
+//How to create strings
+
+let actor = "Denzel Washington"
+let filename = "test.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying
+\"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)\
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+//How to store whole numbers
+
+let score = 10
+
+let reallyBig = 100_0000_0_0_0
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+//How to store decimal numbers
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = a + b
+
+let c = a + Int(b)
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name = "Nicolas Cage"
+name = "John Travolta"
+
+var name = "Nicolas Cage"
+name = 57"
+
+var rating = 5.0
+rating *= 2
+
