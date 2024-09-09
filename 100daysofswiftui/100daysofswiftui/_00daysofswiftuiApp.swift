f771b6cd7738fac2cd88714c307d68d9df0d2d6a
WeMa Intern <wemaintern@iMac-van-Wesley.local>
2024-09-09 09:53:35 +0200

    initial commit

---
+++ 100daysofswiftui/100daysofswiftui/_00daysofswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00daysofswiftuiApp.swift
+//  100daysofswiftui
+//
+//  Created by WeMa Intern on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct _00daysofswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
