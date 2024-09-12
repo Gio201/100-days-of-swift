30a668af5cefa08f2594ea94eb410669580953e4
Giovanni Berlijn <>
2024-09-12 10:43:52 +0200

    Creating pickers in a form

--- 100daysofswiftui/100daysofswiftui/ContentView.swift
+++ 100daysofswiftui/100daysofswiftui/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkamount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkamount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number Of People", selection: $numberOfPeople) {
+                        ForEach(2..<21) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section {
+                    Text(checkamount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
+                }
+            }
+            .navigationTitle("Project 1")
         }
-        .padding()
     }
 }
 
