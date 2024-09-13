63775af049767f85ddb320726064a7a56a4e85b4
Giovanni Berlijn <>
2024-09-13 10:42:44 +0200

    Project 2 - part 2

---
+++ Project 2/Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by WeMa Intern on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
