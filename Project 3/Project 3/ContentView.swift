0f4bf4ff0489ee1f3f6325cbe6960a24dc12f5cd
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-16 11:42:19 +0200

    Project 3 - Challenge 3: Create a custom ViewModifier (and accompanying View extension) that makes a view have a large, blue font suitable for prominent titles in a view.

--- Project 3/Project 3/ContentView.swift
+++ Project 3/Project 3/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, world!")
+            .titleStyle()
     }
 }
 
