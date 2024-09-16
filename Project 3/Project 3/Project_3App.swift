8e268c4f9bedd56587865d047ff145bd258da4e4
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-16 08:32:53 +0200

    added project 3 to the github

---
+++ Project 3/Project 3/Project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3App.swift
+//  Project 3
+//
+//  Created by WeMa Mobile on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
