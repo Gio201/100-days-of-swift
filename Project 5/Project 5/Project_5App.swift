c86aeb4456c3dce7a09ceefa876beba52da1f990
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-17 16:16:20 +0200

    Project 5: Starting Project 5

---
+++ Project 5/Project 5/Project_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5App.swift
+//  Project 5
+//
+//  Created by WeMa Mobile on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
