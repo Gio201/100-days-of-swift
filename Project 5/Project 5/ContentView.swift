14c40ee2afe52aa4459b90c6cdc1aa85d62105e6
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-18 10:05:27 +0200

    update challenge actually removed inserted words now

--- Project 5/Project 5/ContentView.swift
+++ Project 5/Project 5/ContentView.swift
@@ -6,7 +6,7 @@
 //
 // Challenge 1 | Disallow answers that are shorter than three letters or are just our start word. ✅
 //
-// Challenge 2 | Add a toolbar button that calls startGame(), so users can restart with a new word whenever they want to.
+// Challenge 2 | Add a toolbar button that calls startGame(), so users can restart with a new word whenever they want to. ✅
 //
 // Challenge 3 | Put a text view somewhere so you can track and show the player’s score for a given root word. How you calculate score is down to you, but something involving number of words and their letter count would be reasonable.
 ///
@@ -96,6 +96,7 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords.removeAll()
                 return
             }
         }
