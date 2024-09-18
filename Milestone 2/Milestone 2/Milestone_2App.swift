3d3f640ea8e0e0f29483a1d389347c7747f04865
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-18 16:45:41 +0200

    starting with miltestone 2

---
+++ Milestone 2/Milestone 2/Milestone_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_2App.swift
+//  Milestone 2
+//
+//  Created by WeMa Mobile on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
