06babc57d5dace48d893e8931f439be5667527b1
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-20 13:37:53 +0200

    Starting on the "Moonshot" project 👍

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by WeMa Mobile on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
