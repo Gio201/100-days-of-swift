a8b971a7a46b282a77b2563f9c715b012daf17db
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-20 11:20:11 +0200

    Starting on project 7 challenges.

---
+++ Project 7/Project 7/AddView.swift
@@ -0,0 +1,50 @@
+//
+//  AddView.swift
+//  Project 7
+//
+//  Created by WeMa Mobile on 20/09/2024.
+//
+
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+
+    var expenses: Expenses
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "EUR"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ Project 7/Project 7/Project_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7App.swift
+//  Project 7
+//
+//  Created by WeMa Mobile on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
