82f842081e3738ec4ca963ec5e88b3b29cb3248b
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-23 13:27:26 +0200

    Finished the nav tutorial.

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by WeMa Mobile on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
