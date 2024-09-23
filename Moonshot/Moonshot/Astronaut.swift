94599914ffb6d805a460b06dec3dfda83bf5f039
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-23 08:50:57 +0200

    Followed the tutorial.. now I can do the challenges.

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by WeMa Mobile on 20/09/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
