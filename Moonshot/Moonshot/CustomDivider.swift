ecb9bd6d08d6d6f8499ca1d0a050b37dd8c62923
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-23 10:03:45 +0200

    did challenge 1 & 2

---
+++ Moonshot/Moonshot/CustomDivider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDivider.swift
+//  Moonshot
+//
+//  Created by WeMa Mobile on 23/09/2024.
+//
+
+
+import SwiftUI
+
+struct CustomDivider: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundColor(.lightBackground)
+            .padding(.vertical)
+    }
+}
--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -17,71 +17,47 @@ struct MissionView: View {
     let crew: [CrewMember]
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Image(mission.image)
-                    .resizable()
-                    .scaledToFit()
-                    .containerRelativeFrame(.horizontal) { width, axis in
-                        width * 0.6
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    Text(mission.formattedLaunchDate)
+                        .font(.headline.bold())
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .strokeBorder(.white, lineWidth: 0.5)
+                                .opacity(0.5)
+                        )
+                        .padding(.top, 15)
+                    
+                    VStack(alignment: .leading) {
+                        CustomDivider()
+                        
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                        
+                        CustomDivider()
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
                     }
-                
-                VStack(alignment: .leading) {
-                    Rectangle()
-                            .frame(height: 2)
-                            .foregroundStyle(.lightBackground)
-                            .padding(.vertical)
+                    .padding(.horizontal)
                     
-                    Text("Mission Highlights")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                    
-                    Text(mission.description)
-                    
-                    Rectangle()
-                            .frame(height: 2)
-                            .foregroundStyle(.lightBackground)
-                            .padding(.vertical)
-                    
-                    Text("Crew")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                    
+                    CrewRoster(crew: crew)
                 }
-                .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(crew, id: \.role) { CrewMember in
-                            NavigationLink {
-                                AstronautView(astronaut: CrewMember.astronaut)
-                            } label: {
-                                HStack {
-                                    Image(CrewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width: 104, height: 72)
-                                        .clipShape(.capsule)
-                                        .overlay(
-                                            Capsule()
-                                                .strokeBorder(.white, lineWidth: 1)
-                                            )
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(CrewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        
-                                        Text(CrewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                    }
-                                }
-                                .padding(.horizontal)
-                            }
-                        }
-                    }
-                }
+                .padding(.bottom)
             }
-            .padding(.bottom)
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
@@ -90,7 +66,7 @@ struct MissionView: View {
     
     init(mission: Mission, astronauts: [String: Astronaut]) {
         self.mission = mission
-
+        
         self.crew = mission.crew.map { member in
             if let astronaut = astronauts[member.name] {
                 return CrewMember(role: member.role, astronaut: astronaut)
@@ -101,10 +77,12 @@ struct MissionView: View {
     }
 }
 
-#Preview {
-    let missions: [Mission] = Bundle.main.decode("missions.json")
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-
-    return MissionView(mission: missions[0], astronauts: astronauts)
-        .preferredColorScheme(.dark)
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[1], astronauts: astronauts)
+            .preferredColorScheme(.dark)
+    }
 }
