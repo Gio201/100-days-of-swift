322143a88da7812941e06a6600133be0a4ea5b78
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-09-23 16:35:57 +0200

    Starting on "Milestone 3" a habit tracking app

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by WeMa Mobile on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
