843f9f62962990676823906631f6a8ae4db9e092
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-10-02 14:26:49 +0200

    Starting on the BucketList project with maps ect.

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by WeMa Mobile on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
