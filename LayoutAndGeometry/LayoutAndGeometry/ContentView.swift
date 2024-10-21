7f6b33221c4fdda2c9cf52500d63d6a9ea80ecd6
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-10-21 11:14:27 +0200

    Made the views near the top of the scroll view fade out to 0 opacity

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -15,11 +15,15 @@ struct ContentView: View {
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { proxy in
+                        let minY = proxy.frame(in: .global).minY
+                        let opacity = Double(min(max((minY - 200) / 100, 0), 1))
+                        
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
-                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .rotation3DEffect(.degrees(minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(opacity)
                     }
                     .frame(height: 40)
                 }
