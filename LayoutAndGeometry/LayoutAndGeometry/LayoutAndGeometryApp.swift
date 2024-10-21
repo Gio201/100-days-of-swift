539ddf6fdc9b4ad07881fe6740529c6606ae8db9
Giovanni Berlijn <giovanni.berlijn@wemamobile.nl>
2024-10-21 11:07:21 +0200

    Finished the tutorial.

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by WeMa Mobile on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
